faa52d941dd8a49f0a64f56ef2079fa11b4585cc
SalmaAhmed20 <salma.ahmed.anees@stud.cu.eg>
2023-06-23 16:51:59 +0300

    service account

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
 provider "google" {
+  credentials = file("../iti-gcp-project-390712-ca270ab16e9d.json")
   project = var.project_id
   region  = var.region
+}
+terraform {
+  backend "gcs" { #gcs provide automatic state locking
+    bucket = "terraform-backend-anthos2"
+    prefix = "terraform/state"
+  }
 }
--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 gcloud config set project PROJECT_ID
 gcloud services enable compute.googleapis.com &
 gcloud services enable container.googleapis.com &
+export GOOGLE_APPLICATION_CREDENTIALS=/home/salma/gcpp/Anthos-Service-Mesh-GCP-project/iti-gcp-project-390712-ca270ab16e9d.json
