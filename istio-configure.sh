0053f7c166ce5ceee6a7a790bd1d7a267edb5c15
MahmoudMansour <Mahmoud.M.Mansourr@gmail.com>
2023-06-25 01:05:04 +0300

    script

--- istio-configure.sh
+++ istio-configure.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 gcloud services enable mesh.googleapis.com --project=iti-gcp-project-390712
+gcloud container fleet mesh enable --project iti-gcp-project-390712
+gcloud container clusters get-credentials iti-gcp-project-390712-gke --zone us-central1-f --project iti-gcp-project-390712
